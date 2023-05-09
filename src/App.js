72fb2e896f99707f738f0fb534114c6e914f6375
surajNehar <surajneharkar1@gmail.com>
2023-05-09 10:39:23 +0530

    css assignment

--- src/App.js
+++ src/App.js
@@ -1,33 +1,16 @@
 import React from 'react';
 import './App.css'
-import { useRef } from 'react';
-import axios from 'axios'
+import Header from './components/molecules/header/header';
+
+
 
 export default function App() {
-  const apiUrl = 'https://cdn-api.co-vin.in/api/v2/auth/public/generateOTP';
-
-  const inputref = useRef(' ')
-
-  function mobileOtp() {
-    axios({
-      method: 'post',
-      url: 'https://cdn-api.co-vin.in/api/v2/auth/public/generateOTP',
-      data:{
-        mobile:parseInt(inputref.current.value)
-      }
 
 
-    })
-  
-      .then((data) => {
-        console.log(data);
-      })
-      .catch((error) => console.error(error));
-  }
+ 
   return (
     <div>
-      <input type="number" ref={inputref} />
-      <button onClick={mobileOtp}>OTP</button>
+   <Header/>
     </div>
   );
 }
